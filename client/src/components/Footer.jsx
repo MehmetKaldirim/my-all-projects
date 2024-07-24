c0da791626d8af3a96c26696ce668671965453e0
Mehmet <mathdenizi@gmail.com>
2024-07-24 03:45:46 +0200

    signin page and functionality added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -17,10 +17,10 @@ export default function FooterCom() {
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
-              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              <span className="px-2 py-1 bg-gradient-to-r from-purple-500 via-green-500 to-yellow-500 rounded-lg text-white">
                 Math's
               </span>
-              Blog
+              Projects
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
