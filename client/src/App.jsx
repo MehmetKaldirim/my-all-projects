c3d213988ec9c151a76fe8544546a5cc83f3de16
Mehmet <mathdenizi@gmail.com>
2024-07-25 02:06:25 +0200

    project ui page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreateProject from "./pages/CreateProject";
 
 export default function App() {
   return (
@@ -20,6 +22,9 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-project" element={<CreateProject />} />
         </Route>
 
         <Route path="/projects" element={<Projects />} />
