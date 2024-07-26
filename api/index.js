2a420e4e31963f6520c26a7c50be961ca2e5aab2
Mehmet <mathdenizi@gmail.com>
2024-07-26 03:06:26 +0200

    ready for deployment

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import projectRoutes from "./routes/project.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+const port = process.env.PORT || 3000;
 
 dotenv.config();
 
@@ -17,18 +20,26 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/project", projectRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
